14975088ecbd5e44cd80501ef30645d7b6b98960
adem.bulut <adembulutapp@gmail.com>
2025-11-09 23:57:20 +0300

    initial commit

---
+++ irregular_verbsTests/irregular_verbsTests.swift
@@ -0,0 +1,17 @@
+//
+//  irregular_verbsTests.swift
+//  irregular_verbsTests
+//
+//  Created by adem bulut on 9.11.2025.
+//
+
+import Testing
+@testable import irregular_verbs
+
+struct irregular_verbsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
